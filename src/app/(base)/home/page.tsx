141fc9972b0949c7d477c5ab01d9255e81cc7d27
Rahul Patwa <rahul.patwa@automationedge.com>
2024-05-27 17:59:18 +0530

    attempt two

--- src/app/(base)/home/page.tsx
+++ src/app/(base)/home/page.tsx
@@ -4,12 +4,13 @@ import connectDB from "@/app/lib/connectDB";
 import { getServerSession } from "next-auth";
 import "../../globals.css";
 import NewCalender from "./NewCalender";
-import SpeedDial from "./SpeedDial";
 
 async function Home({ searchParams }: { searchParams: any }) {
   const session = await getServerSession();
   await connectDB();
-
+  console.log("searchParams", searchParams);
+  console.log("gte", new Date(searchParams.date).setHours(0, 0, 0, 0));
+  console.log("lte", new Date(searchParams.date).setHours(23, 59, 59, 59));
   const searchDate = new Date(searchParams.date);
 
   const startMonth = new Date(
@@ -23,7 +24,8 @@ async function Home({ searchParams }: { searchParams: any }) {
     searchDate.getMonth() + 1,
     0
   ).setHours(23, 59, 59, 59);
-
+  console.log("startMonth", startMonth);
+  console.log("endMonth", endMonth);
   const query = {
     doctor_email: session?.user?.email,
     visit_start_date: {
@@ -54,7 +56,10 @@ async function Home({ searchParams }: { searchParams: any }) {
   const allPending = completedCount === pendingCount;
 
   return (
-    <div className="px-4 pt-16 h-full pb-[60px] space-y-4 overflow-auto backdrop-blur-3xl" key={`${totalCount}-${pendingCount}-visit`}>
+    <div
+      className="px-4 pt-16 h-full pb-[60px] space-y-4 overflow-auto backdrop-blur-3xl"
+      key={`${totalCount}-${pendingCount}-visit`}
+    >
       <NewCalender visits={plainVisitsMonthly} />
       <VisitTabs
         visits={plainVisits}
